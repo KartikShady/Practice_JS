cdbdcff8898bbe1f880356c287e402cb8284e19e
Kartikey Mishra <42186751+KartikShady@users.noreply.github.com>
2024-02-06 09:36:44 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+// let score = "12abc";
+
+// console.log(typeof score);
+
+// let valueinScore = Number(score);
+// console.log(valueinScore);
+// console.log(typeof(valueinScore));
+
+let isloggedIn = "Adam";
+
+// console.log(isloggedIn);
+// console.log(typeof(isloggedIn));
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+// console.log(typeof(booleanIsloggedIn));
+
+let someNumber = 14;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
